8dfe47cceb584f5552aaca1260e104a080aabc8a
clod10 <claudia.angileri@gmail.com>
2025-06-10 11:54:42 +0200

    lettere

--- sketch.js
+++ sketch.js
@@ -1,3 +1,25 @@
+/** @type {SVGImage[]} */
+let lista_sotto = []
+/** @type {SVGImage[]} */
+let lista_in_mezzo = []
+/** @type {SVGImage[]} */
+let lista_sopra = []
+
+function preload(){
+  lista_sopra = [
+    loadSVG("./assets/sopra/13.svg"),
+    loadSVG("./assets/sopra/13.svg"),
+    loadSVG("./assets/sopra/13.svg"),
+    loadSVG("./assets/sopra/13.svg"),
+    loadSVG("./assets/sopra/13.svg"),
+  ]
+lista_in_mezzo = [
+  loadSVG("./assets/sopra/13.svg"),
+    loadSVG("./assets/sopra/13.svg"),
+    loadSVG("./assets/sopra/13.svg"),
+    loadSVG("./assets/sopra/13.svg"),
+    loadSVG("./assets/sopra/13.svg"),
+]}
 function setup() {
   createCanvas(400, 400, SVG);
   addDownloadButton();
